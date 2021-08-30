5c61a57c7bc11a717805689729ec21439c93a36d
tel3gramb0t <89654703+tel3gramb0t@users.noreply.github.com>
2021-08-30 15:53:09 +0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -266,9 +266,9 @@ except KeyError:
 try:
     IMAGE_URL = getConfig('IMAGE_URL')
     if len(IMAGE_URL) == 0:
-        IMAGE_URL = 'https://telegra.ph/file/019996f816db9ed576cff.jpg'
+        IMAGE_URL = 'https://github.com/tel3gramb0t/Torrent_2_Gdrive_mirrorbot/blob/master/images.jpeg'
 except KeyError:
-    IMAGE_URL = 'https://telegra.ph/file/019996f816db9ed576cff.jpg'
+    IMAGE_URL = 'https://github.com/tel3gramb0t/Torrent_2_Gdrive_mirrorbot/blob/master/images.jpeg'
 try:
     STOP_DUPLICATE = getConfig('STOP_DUPLICATE')
     if STOP_DUPLICATE.lower() == 'true':
