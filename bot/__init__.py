80c7747900adc7b0b8989f3bd8d528d0504124f1
kangupdate <90373364+kangupdate@users.noreply.github.com>
2021-09-27 14:58:23 +0700

    Add files via upload

--- bot/__init__.py
+++ bot/__init__.py
@@ -266,9 +266,9 @@ except KeyError:
 try:
     IMAGE_URL = getConfig('IMAGE_URL')
     if len(IMAGE_URL) == 0:
-        IMAGE_URL = 'https://www.finetoshine.com/wp-content/uploads/2020/07/itachi__s_sharingan_by_kago_woogif-600%C3%97450.gif'
+        IMAGE_URL = 'https://telegra.ph/file/019996f816db9ed576cff.jpg'
 except KeyError:
-    IMAGE_URL = 'https://www.finetoshine.com/wp-content/uploads/2020/07/itachi__s_sharingan_by_kago_woogif-600%C3%97450.gif'
+    IMAGE_URL = 'https://telegra.ph/file/019996f816db9ed576cff.jpg'
 try:
     STOP_DUPLICATE = getConfig('STOP_DUPLICATE')
     if STOP_DUPLICATE.lower() == 'true':
